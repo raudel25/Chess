34a8033e2fac50ed812e18d02e8e4d6052b6a68b
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2022-10-18 09:26:05 -0400

    Act evaluate

--- chess_game/evaluate_state.py
+++ chess_game/evaluate_state.py
@@ -41,18 +41,18 @@ def position_determinate(pieces, position: list, white: bool):
     score: int = 0
 
     for i in pieces:
-        score += position[i] if white else position[len(position) - 1 - i]
+        score += position[i] if not white else position[len(position) - 1 - i]
 
     return score
 
 
 position_pawns = [0, 0, 0, 0, 0, 0, 0, 0,
                   50, 50, 50, 50, 50, 50, 50, 50,
-                  10, 10, 20, 30, 30, 20, 10, 10,
-                  5, 5, 10, 25, 25, 10, 5, 5,
-                  0, 0, 0, 20, 20, 0, 0, 0,
+                  10, 10, 20, 45, 35, 20, 10, 10,
+                  5, 5, 10, 42, 42, 10, 5, 5,
+                  0, 0, 0, 40, 40, 0, 0, 0,
                   5, -5, -10, 0, 0, -10, -5, 5,
-                  5, 10, 10, -20, -20, 10, 10, 5,
+                  5, 10, 10, -50, -50, 10, 10, 5,
                   0, 0, 0, 0, 0, 0, 0, 0]
 
 position_knights = [-50, -40, -30, -30, -30, -30, -40, -50,
