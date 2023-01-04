8e60658981f2af2f11646299e838eb5a8c98e316
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-01-03 22:56:35 -0500

    best_str_board

--- main.py
+++ main.py
@@ -25,9 +25,31 @@ def select_player(color: str) -> strategy.Strategy:
             return players[select]()
 
 
+def best_str_board(board) -> str:
+    str_board: str = str(board)
+    s: str = '   '
+
+    for i in range(8):
+        aux = chr(ord('a') + i)
+        s = f'{s}{aux} '
+    s = f'{s}\n\n'
+
+    for i in range(8):
+        s = f'{s}{8 - i}  {str_board[i * 16:i * 16 + 15]}  {8 - i}\n'
+
+    s = f'{s}\n   '
+
+    for i in range(8):
+        aux = chr(ord('a') + i)
+        s = f'{s}{aux} '
+
+    return s
+
+
 my_game: game.Game = game.Game(select_player('blanco'), select_player('negro'))
 
 for i in my_game.run_game():
     os.system('clear')
-    print(i)
+    print(best_str_board(i))
+    print()
     time.sleep(1)
